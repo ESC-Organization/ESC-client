64413f92469986d1081610580fa2afa7160a8026
TaegeonKim <aksen5240@gmail.com>
2024-10-07 14:06:58 +0900

    chore: Apply DNFBitBitv2 font with Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ function App() {
   return (
     <div className="App">
       <img src="/src/assets/react.svg" alt="react" />
-      <div className="text-3xl font-bold underline text-blue-500">
+      <div className="text-3xl underline text-blue-500">
         Hello ESC Project!!
+      </div>
+      <div className="text-3xl text-blue-500">
+        안녕 ESC 프로젝트!!
       </div>
     </div>
   );
