a496a7fbae9c4eaca26eece1f0e7647dac067c50
TaegeonKim <aksen5240@gmail.com>
2024-10-07 15:32:14 +0900

    chore: Set up absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-function App() {
+import TestPage from "@/pages/test-page";
+
+export default function App() {
   return (
-    <div className="App">
-      <img src="/src/assets/react.svg" alt="react" />
-      <div className="text-3xl underline text-blue-500">
-        Hello ESC Project!!
-      </div>
-      <div className="text-3xl text-blue-500">
-        안녕 ESC 프로젝트!!
-      </div>
+    <div>
+      hello
+      <TestPage />
     </div>
   );
 }
-
-export default App;
