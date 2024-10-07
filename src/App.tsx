588bd3f584c56df14fa8a1ade485f04ef82b9b66
TaegeonKim <aksen5240@gmail.com>
2024-10-07 13:41:21 +0900

    chore: Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <img src="/src/assets/react.svg" alt="react" />
+      <div className="text-3xl font-bold underline text-blue-500">
+        Hello ESC Project!!
+      </div>
+    </div>
+  );
+}
+
+export default App;
