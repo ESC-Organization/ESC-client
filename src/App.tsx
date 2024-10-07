0192804000eb414ff3bbf2eca4874a8a95792d4c
TaegeonKim <aksen5240@gmail.com>
2024-10-07 16:27:35 +0900

    chore: Set up ESLint and Prettier configuration in detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TestPage from "@/pages/test-page";
-import Main from "@/pages/main";
-import Onboarding from "@/pages/onboarding";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import TestPage from '@/pages/test-page';
+import Main from '@/pages/main-page';
+import Onboarding from '@/pages/onboarding';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Main />,
   },
   {
-    path: "/onboarding",
+    path: '/onboarding',
     element: <Onboarding />,
   },
   {
-    path: "/test",
+    path: '/test',
     element: <TestPage />,
   },
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
