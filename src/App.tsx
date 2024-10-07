dbfc590cfb79a36579a3a55081bcfcfd12fb117c
TaegeonKim <aksen5240@gmail.com>
2024-10-07 15:45:23 +0900

    chore: Set up routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import TestPage from "@/pages/test-page";
+import Main from "@/pages/main";
+import Onboarding from "@/pages/onboarding";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/onboarding",
+    element: <Onboarding />,
+  },
+  {
+    path: "/test",
+    element: <TestPage />,
+  },
+]);
 
 export default function App() {
-  return (
-    <div>
-      hello
-      <TestPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
