bc84b2201e6bc9e98334854578e0a3f956cc329d
jllee000 <jllee000@naver.com>
2024-10-22 17:33:14 +0900

    feat : 퀴즈 사운드 토클

--- src/component/answer/Subject.tsx
+++ src/component/answer/Subject.tsx
@@ -1,59 +1,33 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Monkey from '/src/assets/images/items/monkey.png';
-import { useState } from 'react';
-import Monkey from '/src/assets/images/items/monkey.png';
+
 interface ObjectProps {
   q: React.ReactNode;
   onSubject: (subject: string) => void;
   onClose: () => void;
-  onSubject: (subject: string) => void;
-  onClose: () => void;
 }
+
 export default function Subject({ q, onSubject, onClose }: ObjectProps) {
   const [inputValue, setInputValue] = useState('');
+  const [isHint, setIsHint] = useState(false);
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
-  const [isHint, setIsHint] = useState(false);
 
-  // 제출 버튼을 클릭했을 때 호출되는 함수
   const handleSubmit = () => {
     console.log(inputValue);
     onSubject(inputValue); // 입력된 값 외부로 전달
   };
-  const handleClose = () => {
-    onClose(); // 입력된 값 외부로 전달
-  };
-  const showHint = () => {
-    if (isHint == true) {
-      setIsHint(false);
-    } else {
-      setIsHint(true);
-    }
-  };
-export default function Subject({ q, onSubject, onClose }: ObjectProps) {
-  const [inputValue, setInputValue] = useState('');
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
-  const [isHint, setIsHint] = useState(false);
 
-  // 제출 버튼을 클릭했을 때 호출되는 함수
-  const handleSubmit = () => {
-    console.log(inputValue);
-    onSubject(inputValue); // 입력된 값 외부로 전달
-  };
   const handleClose = () => {
-    onClose(); // 입력된 값 외부로 전달
+    onClose();
   };
+
   const showHint = () => {
-    if (isHint == true) {
-      setIsHint(false);
-    } else {
-      setIsHint(true);
-    }
+    setIsHint(!isHint);
   };
+
   return (
     <div className="flex flex-col p-2 gap-2 w-full h-full transform relative z-[80] bg-[#00000059] flex justify-center items-center">
       <div className="relative p-4 text-white border-4 border-[#606060] w-[90%] bg-[#404040]">
@@ -63,7 +37,7 @@ export default function Subject({ q, onSubject, onClose }: ObjectProps) {
         >
           {isHint && (
             <div className="fixed w-[57%] h-[12%] z-[100]">
-              <div className="absolute right-[29%] -top-[110%] text-center w-full p-2 bg-white border-4 border-[#808080]  rounded-[5px]">
+              <div className="absolute right-[29%] -top-[110%] text-center w-full p-2 bg-white border-4 border-[#808080] rounded-[5px]">
                 <span
                   style={{ WebkitTextStroke: '1px #000' }}
                   className="text-white text-[0.9rem]"
@@ -72,39 +46,11 @@ export default function Subject({ q, onSubject, onClose }: ObjectProps) {
                   <br />
                   흑백요리사 마지막화를 봐봐
                 </span>
-              </div>{' '}
+              </div>
             </div>
           )}
-
-      <div className="relative p-4 text-white border-4 border-[#606060] w-[90%] bg-[#404040]">
-        <div
-          className="absolute -top-[25%] w-[30%] left-[35%] -z-[10]"
-          onClick={showHint}
-        >
-          {isHint && (
-            <div className="fixed w-[57%] h-[12%] z-[100]">
-              <div className="absolute right-[29%] -top-[110%] text-center w-full p-2 bg-white border-4 border-[#808080]  rounded-[5px]">
-                <span
-                  style={{ WebkitTextStroke: '1px #000' }}
-                  className="text-white text-[0.9rem]"
-                >
-                  힌트
-                  <br />
-                  흑백요리사 마지막화를 봐봐
-                </span>
-              </div>{' '}
-            </div>
-          )}
-
-          <img src={Monkey} />
+          <img src={Monkey} alt="Monkey" />
         </div>
-        <div
-          className="text-right text-[1.1rem] pr-2 cursor-pointer -mt-[2%]"
-          onClick={handleClose}
-        >
-          x
-        </div>
-        <div className="mt-[2%]">{q}</div>
         <div
           className="text-right text-[1.1rem] pr-2 cursor-pointer -mt-[2%]"
           onClick={handleClose}
@@ -113,20 +59,16 @@ export default function Subject({ q, onSubject, onClose }: ObjectProps) {
         </div>
         <div className="mt-[2%]">{q}</div>
         <div className="p-2 mt-[5%] mb-[5%] flex flex-col w-full gap-4">
-          <div className="p-2 rounded-[5px] bg-[#FFFFFF] w-full mx-auto  border-[4px] border-[#808080]">
+          <div className="p-2 rounded-[5px] bg-[#FFFFFF] w-full mx-auto border-[4px] border-[#808080]">
             <input
               className="placeholder-white bg-none"
               style={{ WebkitTextStroke: '1px #000' }}
               placeholder="정답을 적어주세요"
               onChange={handleInputChange}
-              onChange={handleInputChange}
+              value={inputValue}
             />
           </div>
         </div>
-        <div className="mt-[5%] mb-[10%] text-right">
-          <span style={{ WebkitTextStroke: '1px #000' }} onClick={handleSubmit}>
-            제출하기 {`>`}
-          </span>
         <div className="mt-[5%] mb-[10%] text-right">
           <span style={{ WebkitTextStroke: '1px #000' }} onClick={handleSubmit}>
             제출하기 {`>`}
--- src/component/bar/TopBar.tsx
+++ src/component/bar/TopBar.tsx
@@ -1,6 +1,21 @@
 import Coin from '/src/assets/images/items/coin.png';
 import Sound from '/src/assets/images/items/sound.png';
-export default function TopBar() {
+import SoundOff from '/src/assets/images/items/sound-off.png';
+import { useState } from 'react';
+interface BarComponent {
+  onSound: (number: number) => void;
+}
+export default function TopBar({ onSound }: BarComponent) {
+  const [isSound, setIsSound] = useState(1);
+  const toggleSound = () => {
+    if (isSound == 0) {
+      setIsSound(1);
+      onSound(1);
+    } else {
+      setIsSound(0);
+      onSound(0);
+    }
+  };
   return (
     <div className="absolute top-0 w-full pt-4 z-[90] pr-4">
       <div className="relative gap-[1rem] items-center flex">
@@ -10,8 +25,8 @@ export default function TopBar() {
           </div>
           <div className="text-white text-[1.2rem]"> x 1</div>
         </div>
-        <div className="w-[8%]">
-          <img src={Sound} className="w-full" />
+        <div className="w-[8%]" onClick={toggleSound}>
+          <img src={isSound === 1 ? Sound : SoundOff} className="w-full" />
         </div>
       </div>
     </div>
