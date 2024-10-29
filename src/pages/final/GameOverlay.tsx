ab31ea9a85801ef8e6dc790ddb44cfdba3bc8b00
daram62 <97465078+daram62@users.noreply.github.com>
2024-10-30 03:03:06 +0900

    chore: lint error fix in final

--- src/pages/final/GameOverlay.tsx
+++ src/pages/final/GameOverlay.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 interface GameOverlayProps {
   status: 'start' | 'clear' | 'over';
@@ -14,13 +14,17 @@ export default function GameOverlay({ status, onFinish }: GameOverlayProps) {
   }, [status, onFinish]);
 
   const overlayText =
-    status === 'start' ? '- Game Start! -' :
-    status === 'clear' ? '- Game Clear! -' :
-    '- Game Over -';
+    status === 'start'
+      ? '- Game Start! -'
+      : status === 'clear'
+        ? '- Game Clear! -'
+        : '- Game Over -';
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-1/4 z-30 space-y-8">
-      <h1 className="text-white text-4xl sm:text-5xl font-bold">{overlayText}</h1>
+      <h1 className="text-white text-4xl sm:text-5xl font-bold">
+        {overlayText}
+      </h1>
     </div>
   );
 }
